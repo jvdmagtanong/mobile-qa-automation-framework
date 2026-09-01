ee4e9495f8fdf88e5ebb3246bbe3e55e8386cce3
jvdmagtanong <jvdmagtanong@gmail.com>
2026-08-31 21:46:49 -0700

    Update header_page.py

--- pages/model/header_page.py
+++ pages/model/header_page.py
@@ -4,6 +4,13 @@ from pages.locator.header_locator import HeaderLocator
 
 class HeaderPage(BasePage):
 
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.wait_for_app_logo()
+
+    def wait_for_app_logo(self):
+        self.wait_for_element_visible(HeaderLocator.APP_LOGO_AND_NAME)
+
     def open_menu(self):
         self.wait_for_element_visible(HeaderLocator.MENU_BUTTON)
         self.click_element(HeaderLocator.MENU_BUTTON)
